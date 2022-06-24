70020a934419b4ea736116d9930d0c0ee8580c7a
james123lisenby-ALgDYk <james123lisenby@hotmail.com>
2022-06-23 21:24:25 -0700

    first commit

---
+++ Project5.js
@@ -0,0 +1,18 @@
+// Declare Time
+
+// Print the Date
+
+// Declare Current Day Element- Class = Container
+
+// Declare Time Entries Element
+
+// Render a block for each hour of the day 9-5
+
+
+var myArray = ["dick", "hole"]
+for(var i=0; i <= 17; i++);
+// Pull the data for the hour of the loop
+
+
+// Save an hour to local storage
+
