cb9909928cfd130d7b915b9dc6053a9f49750f9c
james123lisenby-ALgDYk <james123lisenby@hotmail.com>
2022-06-26 23:51:41 -0700

    changed text area to input

--- js.js
+++ js.js
@@ -3,3 +3,6 @@ var today = new Date();
 var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
 
 document.getElementById("currentDay").innerHTML = today;
+// Displays the date and time in the jumbotron.
+
+
