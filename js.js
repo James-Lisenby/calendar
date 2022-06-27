96259afa9ac80a32b990cf035768e771b8447564
james123lisenby-ALgDYk <james123lisenby@hotmail.com>
2022-06-27 11:15:53 -0700

    added some psuedo code. Id'd the rows by hour of the day.

--- js.js
+++ js.js
@@ -6,3 +6,12 @@ document.getElementById("currentDay").innerHTML = today;
 // Displays the date and time in the jumbotron.
 
 
+
+// add event listener to "save" button.
+// store user input into local storage as a string upon hitting the "save" button.
+
+
+// retrieve data on page load.
+
+// set if hour is past,present, or future.
+
