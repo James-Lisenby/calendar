984f50c8a86ba62255ff06ff9a1399f2100e7ed9
james123lisenby-ALgDYk <james123lisenby@hotmail.com>
2022-06-27 17:37:57 -0700

    is this hard or am i dumb

--- js.js
+++ js.js
@@ -5,6 +5,14 @@ var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
 document.getElementById("currentDay").innerHTML = today;
 // Displays the date and time in the jumbotron.
 
+
+// Remder a block for each hour of the planner.
+for(var i =9; i <= 17; i++) {
+
+}
+
+
+
 const button = document.querySelector("button");
 button.addEventListener('click', getInputValue);
 
@@ -13,3 +21,4 @@ function getInputValue() {
     console.log(inputVal);
     document.getElementById("userInput").innerHTML = inputVal;
 }
+// Currently function will console log user input. 
