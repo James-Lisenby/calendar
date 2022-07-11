261c728635bae792c328b320bfd3b0288350d394
james123lisenby-ALgDYk <james123lisenby@hotmail.com>
2022-07-10 18:45:16 -0700

    cleaned up JS code

--- assets/Java/js.js
+++ assets/Java/js.js
@@ -1,6 +1,6 @@
 var today = new Date();
 
-var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+var date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
 
 document.getElementById("currentDay").innerHTML = today;
 // Displays the date and time in the jumbotron.
@@ -16,17 +16,17 @@ console.log(blockOfTime);
 
 // Needs to apply to the text area "children" of the time block.
 
-function hourColor(){
+function hourColor() {
     for (let index = 0; index < blockOfTime.length; index++) {
         var hour = blockOfTime[index].dataset.hour;
-        if(currentTime > hour){
+        if (currentTime > hour) {
             blockOfTime[index].firstElementChild.classList.add("past")
-        }else if(currentTime == hour){
+        } else if (currentTime == hour) {
             blockOfTime[index].firstElementChild.classList.add("present")
-        }else if(currentTime < hour){
+        } else if (currentTime < hour) {
             blockOfTime[index].firstElementChild.classList.add("future")
         }
-        
+
     };
 
 }
@@ -61,13 +61,11 @@ var saveBtn9 = document.getElementById('saveInput-9');
 function getInput1() {
     var userInput1 = text1.value;
     localStorage.setItem("userInput1", JSON.stringify(userInput1));
-    console.log(userInput1);
 };
 
 function displayInput1() {
     var textInput1 = JSON.parse(localStorage.getItem("userInput1"));
-    console.log(textInput1);
-    document.getElementById("input-1").innerHTML = textInput1;  
+    document.getElementById("input-1").innerHTML = textInput1;
 
 };
 
@@ -75,104 +73,88 @@ function displayInput1() {
 function getInput2() {
     var userInput2 = text2.value;
     localStorage.setItem("userInput2", JSON.stringify(userInput2));
-    console.log(userInput2);
 };
 
 function displayInput2() {
     var textInput2 = JSON.parse(localStorage.getItem("userInput2"));
-    console.log(textInput2);
-    document.getElementById("input-2").innerHTML = textInput2;  
+    document.getElementById("input-2").innerHTML = textInput2;
 
 };
 
 function getInput3() {
     var userInput3 = text3.value;
     localStorage.setItem("userInput3", JSON.stringify(userInput3));
-    console.log(userInput3);
 };
 
 function displayInput3() {
     var textInput3 = JSON.parse(localStorage.getItem("userInput3"));
-    console.log(textInput3);
-    document.getElementById("input-3").innerHTML = textInput3;  
+    document.getElementById("input-3").innerHTML = textInput3;
 
 };
 
 function getInput4() {
     var userInput4 = text4.value;
     localStorage.setItem("userInput4", JSON.stringify(userInput4));
-    console.log(userInput4);
 };
 
 function displayInput4() {
     var textInput4 = JSON.parse(localStorage.getItem("userInput4"));
-    console.log(textInput4);
-    document.getElementById("input-4").innerHTML = textInput4;  
+    document.getElementById("input-4").innerHTML = textInput4;
 
 };
 
 function getInput5() {
     var userInput5 = text5.value;
     localStorage.setItem("userInput5", JSON.stringify(userInput5));
-    console.log(userInput5);
 };
 
 function displayInput5() {
     var textInput5 = JSON.parse(localStorage.getItem("userInput5"));
-    console.log(textInput5);
-    document.getElementById("input-5").innerHTML = textInput5;  
+    document.getElementById("input-5").innerHTML = textInput5;
 
 };
 
 function getInput6() {
     var userInput6 = text6.value;
     localStorage.setItem("userInput6", JSON.stringify(userInput6));
-    console.log(userInput6);
 };
 
 function displayInput6() {
     var textInput6 = JSON.parse(localStorage.getItem("userInput6"));
-    console.log(textInput6);
-    document.getElementById("input-6").innerHTML = textInput6;  
+    document.getElementById("input-6").innerHTML = textInput6;
 
 };
 
 function getInput7() {
     var userInput7 = text7.value;
     localStorage.setItem("userInput7", JSON.stringify(userInput7));
-    console.log(userInput7);
 };
 
 function displayInput7() {
     var textInput7 = JSON.parse(localStorage.getItem("userInput7"));
-    console.log(textInput7);
-    document.getElementById("input-7").innerHTML = textInput7;  
+    document.getElementById("input-7").innerHTML = textInput7;
 
 };
 
 function getInput8() {
     var userInput8 = text8.value;
     localStorage.setItem("userInput8", JSON.stringify(userInput8));
-    console.log(userInput8);
 };
 
 function displayInput8() {
     var textInput8 = JSON.parse(localStorage.getItem("userInput8"));
-    console.log(textInput8);
-    document.getElementById("input-8").innerHTML = textInput8;  
+    document.getElementById("input-8").innerHTML = textInput8;
 
 };
 
 function getInput9() {
     var userInput9 = text9.value;
     localStorage.setItem("userInput9", JSON.stringify(userInput9));
-    console.log(userInput9);
 };
 
 function displayInput9() {
     var textInput9 = JSON.parse(localStorage.getItem("userInput9"));
-    console.log(textInput9);
-    document.getElementById("input-9").innerHTML = textInput9;  
+    document.getElementById("input-9").innerHTML = textInput9;
 }
 
 displayInput1();
